c3ba99a8577b22e3202258b447cd18d8e2e2c6ba
agarictree <ormhild@gmail.com>
2022-02-23 19:54:17 +0300

    feat: add solution1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = {};
+  bracketsConfig.map(elem => obj[elem[1]] = elem[0]);
+  
+  console.log(str, bracketsConfig);
+  let stack = [];
+  let splittedStr = str.split("");
+  
+  let keys = Object.keys(obj);
+  for(let i = 0; i < splittedStr.length; i++) {
+    if(keys.includes(splittedStr[i])) {  
+      if(obj[splittedStr[i]] !== stack.pop() && stack.pop() != undefined) return false;
+    } else {
+      stack.push(splittedStr[i]);
+    }
+  }
+  return stack.length == 0;
 }
